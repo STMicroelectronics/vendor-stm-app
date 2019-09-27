c49380ea4ae02438ae12adfc72b0443df14eb71e
Jean-Philippe ROMAIN <jean-philippe.romain@st.com>
2019-09-27 17:41:42 +0200

    st-android-9.0.0-2019-09-27 (v1.0.0)
    Change-Id: Ibc6a5dfc91db411b70382ae74394ae685dcf7b4d

---
+++ Android.mk
@@ -0,0 +1,88 @@
+# 
+# Copyright (C) 2019 STMicroelectronics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Install the prebuilt ST apk
+LOCAL_PATH := $(call my-dir)
+
+# ST Copro M4 Echo prebuilt application
+# Sources available in gerrit : android-brillo-mpu/app_stcoprom4echo
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := STCoproM4Echo
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+# ST Copro M4 Example prebuilt application
+# Sources available in gerrit : android-brillo-mpu/app_stcoprom4example
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := STCoproM4Example
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+# ST Perf view prebuilt application
+# Sources available in gerrit : android-brillo-mpu/app_stperf
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := STPerf
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+# ST Video application
+# Sources available in gerrit : android-brillo-mpu/app_stvideo
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := STVideo
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+# ST Camera prebuilt application
+# Sources available in gerrit : android-brillo-mpu/app_stcamera
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := STCamera
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_PREBUILT)
+
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# vendor-stm-app
-STM32MPU distribution for Android (applications)
+# Applications #
+
+This module contains the STMicroelectronics applications associated if required to their respective coprocessor firmware.
+
+It is part of the STMicroelectronics Android delivery (see the [delivery][] for more information).
+
+[delivery]: https://wiki.st.com/stm32mpu/wiki/STM32MP15_distribution_for_Android_release_note_-_v1.0.0
+
+## Description ##
+
+This module version is the first version for stm32mp1
+
+Please see the Android delivery release notes for more details.
+
+## Documentation ##
+
+* The [release notes][] document the information on the release.
+* The [distribution package][] provides detailed information on how to use this delivery.
+
+[release notes]: https://wiki.st.com/stm32mpu/wiki/STM32MP15_distribution_for_Android_release_note_-_v1.0.0
+[distribution package]: https://wiki.st.com/stm32mpu/wiki/STM32MP1_Distribution_Package_for_Android
+
+## Dependencies ##
+
+This module can't be used alone. It is part of the STMicroelectronics Android delivery.
+
+## Containing ##
+
+This module contains application packages and their associated coprocessor firmwares.
+
+**Makefile:**
+* `Android.mk`: used to integrate optionally the application packages in the device build
+
+**Applications:**
+* `STCoproM4Echo.apk`: coprocessor service example (echo use case)
+* `STCoproM4Example.apk`: coprocessor service example (ADC/DAC use case + several resources usage)
+* `STCamera.apk`: basic camera application
+* `STVideo.apk`: basic video application
+* `STPerf.apk`: performance view overlay
+
+**Firmwares (in ./firmwares/):**
+* `OpenAMP_TTY_echo.elf`: firmware binary associated to STCoproM4Echo application
+* `copro_m4example.elf`: firmware binary used associated to STCoproM4Example application
+
+## License ##
+
+This module is distributed under the Apache License, Version 2.0 found in the [LICENSE](./LICENSE) file.
